25ae8c0080cd499b57c3ff40bd84b00d87c55a1b
ZafarUrakov <goliburakov535@gmail.com>
2024-01-29 08:17:06 +0300

    BROKERS: Get Error Code

---
+++ STX.EFxceptions.MySql.Base/Brokers/DbErrorBroker/IMySqlErrorBroker.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using MySql.Data.MySqlClient;
+using STX.EFxceptions.Interfaces.Brokers.DbErrorBroker;
+
+namespace STX.EFxceptions.MySql.Base.Brokers.DbErrorBroker
+{
+    public interface IMySqlErrorBroker : IDbErrorBroker<MySqlException>
+    { }
+}
---
+++ STX.EFxceptions.MySql.Base/Brokers/DbErrorBroker/MySqlErrorBroker.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using MySql.Data.MySqlClient;
+
+namespace STX.EFxceptions.MySql.Base.Brokers.DbErrorBroker
+{
+    public class MySqlErrorBroker : IMySqlErrorBroker
+    {
+        public int GetErrorCode(MySqlException mySqlException) =>
+            mySqlException.ErrorCode;
+    }
+}
