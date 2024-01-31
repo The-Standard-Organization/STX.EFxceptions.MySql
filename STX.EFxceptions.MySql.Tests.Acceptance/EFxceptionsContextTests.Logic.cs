56036b28dd67720da164e707e52ae36db04fe86a
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-02-01 01:42:14 +0900

    ShouldSaveChangesSuccessfully -> PASS

---
+++ STX.EFxceptions.MySql.Tests.Acceptance/EFxceptionsContextTests.Logic.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using STX.EFxceptions.MySql.Tests.Acceptance.Models.Clients;
+using Xunit;
+
+namespace STX.EFxceptions.MySql.Tests.Acceptance
+{
+    public partial class EFxceptionsContextTests
+    {
+        [Fact]
+        public void ShouldSaveChangesSuccessfully()
+        {
+            // given
+            var client = new Client
+            {
+                Id = Guid.NewGuid()
+            };
+
+            // when
+            context.Clients.Add(client);
+            context.SaveChanges();
+
+            // then
+            context.Clients.Remove(client);
+            context.SaveChanges();
+        }
+    }
+}
