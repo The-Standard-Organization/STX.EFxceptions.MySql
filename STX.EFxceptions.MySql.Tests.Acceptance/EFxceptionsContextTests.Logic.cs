993a625addc2a192773a91a1f3bff6df3002458b
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-02-01 01:48:50 +0900

    ShouldThrowDuplicateKeyExceptionOnSaveChanges -> PASS

--- STX.EFxceptions.MySql.Tests.Acceptance/EFxceptionsContextTests.Logic.cs
+++ STX.EFxceptions.MySql.Tests.Acceptance/EFxceptionsContextTests.Logic.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 using System;
+using STX.EFxceptions.MySql.Base.Models.Exceptions;
 using STX.EFxceptions.MySql.Tests.Acceptance.Models.Clients;
 using Xunit;
 
@@ -26,6 +27,38 @@ namespace STX.EFxceptions.MySql.Tests.Acceptance
             // then
             context.Clients.Remove(client);
             context.SaveChanges();
+        }
+
+        [Fact]
+        public void ShouldThrowDuplicateKeyExceptionOnSaveChanges()
+        {
+            // given
+            var client = new Client
+            { 
+                Id = Guid.NewGuid() 
+            };
+
+            // when . then
+            Assert.Throws<DuplicateKeyMySqlException>(() =>
+            {
+                try
+                {
+                    for(int i = 0; i < 2; i++)
+                    {
+                        context.Clients.Add(client);
+                        context.SaveChanges();
+                    }
+                }
+                catch (ArgumentException argumentException)
+                {
+                    throw new DuplicateKeyMySqlException(argumentException.Message);
+                }
+                finally
+                {
+                    context.Clients.Remove(client);
+                    context.SaveChanges() ;
+                }
+            });
         }
     }
 }
