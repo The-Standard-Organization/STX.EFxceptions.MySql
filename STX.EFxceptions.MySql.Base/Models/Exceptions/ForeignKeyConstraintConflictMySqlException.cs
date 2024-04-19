e4c11cf1a648c9cb7aff0b19f62825200efc9fa9
ZafarUrakov <goliburakov535@gmail.com>
2024-04-19 16:54:57 +0500

    CODE RUB: Change Exceptions -> DbUpdateException

--- STX.EFxceptions.MySql.Base/Models/Exceptions/ForeignKeyConstraintConflictMySqlException.cs
+++ STX.EFxceptions.MySql.Base/Models/Exceptions/ForeignKeyConstraintConflictMySqlException.cs
@@ -2,11 +2,11 @@
 // Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace STX.EFxceptions.MySql.Base.Models.Exceptions
 {
-    public class ForeignKeyConstraintConflictMySqlException : Exception
+    public class ForeignKeyConstraintConflictMySqlException : DbUpdateException
     {
         public ForeignKeyConstraintConflictMySqlException(string message) : base(message) { }
     }
